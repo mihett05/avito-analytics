6cbaa7b8d087d6d1cc20863690717bb25ba04f00
AlexandrVino <alexander-vin0@yandex.ru>
2024-03-09 19:27:37 +0300

    update database structure

--- backend/models/schema.py
+++ backend/models/schema.py
@@ -9,9 +9,9 @@ class Base(DeclarativeBase):
 class Locations(Base):
     __tablename__ = "locations"
 
-    id: Mapped[int] = mapped_column(INT, primary_key=True)
-    key: Mapped[str] = mapped_column(String())
-    name: Mapped[str] = mapped_column(String())
+    id: Mapped[int] = mapped_column(primary_key=True)
+    key: Mapped[str] = mapped_column()
+    name: Mapped[str] = mapped_column()
 
     parent_id: Mapped[int] = mapped_column(ForeignKey("locations.id"))
 
@@ -19,9 +19,9 @@ class Locations(Base):
 class Categories(Base):
     __tablename__ = "categories"
 
-    id: Mapped[int] = mapped_column(INT, primary_key=True)
-    key: Mapped[str] = mapped_column(String())
-    name: Mapped[str] = mapped_column(String())
+    id: Mapped[int] = mapped_column(primary_key=True)
+    key: Mapped[str] = mapped_column()
+    name: Mapped[str] = mapped_column()
 
     parent_id: Mapped[int] = mapped_column(ForeignKey("categories.id"))
 
@@ -29,7 +29,7 @@ class Categories(Base):
 class Prices(Base):
     __tablename__ = "prices"
 
-    price: Mapped[int] = mapped_column(INT)
+    price: Mapped[int] = mapped_column()
     matrix_id: Mapped[int] = mapped_column(ForeignKey("matrices.id"), primary_key=True)
     location_id: Mapped[int] = mapped_column(ForeignKey("locations.id"), primary_key=True)
     microcategory_id: Mapped[int] = mapped_column(ForeignKey("categories.id"), primary_key=True)
@@ -38,7 +38,7 @@ class Prices(Base):
 class Matrices(Base):
     __tablename__ = "matrices"
 
-    id: Mapped[int] = mapped_column(INT, primary_key=True)
-    name: Mapped[str] = mapped_column(String())
-    segment_id: Mapped[int] = mapped_column(INT, nullable=True)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column()
+    segment_id: Mapped[int] = mapped_column(nullable=True)
 
