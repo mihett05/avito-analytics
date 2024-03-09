5119501c731cd4037ec09440068a00a434d58c8f
AlexandrVino <alexander-vin0@yandex.ru>
2024-03-09 20:02:33 +0300

    move api.py to routers/

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from api import api
+from routers import api
 from models.models import init_models
 
 
--- backend/routers/__init__.py
+++ backend/routers/__init__.py
@@ -1,3 +1,4 @@
 from fastapi import APIRouter
 
 api = APIRouter()
+# include routers here
