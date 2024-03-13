33731642b8abc84b0cea5c475a7919a4a39e947a
AlexandrVino <alexander-vin0@yandex.ru>
2024-03-13 17:19:53 +0300

    analytics dev

--- backend/storage/analytics.py
+++ backend/storage/analytics.py
@@ -1,8 +1,23 @@
 from redis.asyncio import Redis
 
 
-async def get_storage_conf(client: Redis):
-    pass
+async def update_price_target_requests(client: Redis):
+    if not await client.hget('$', 'analytics'):
+        await client.hset('analytics', mapping={
+            "price_target_requests": 0,
+            "locations": {},
+            "categories": {},
+        })
+
+    await client.hincrby(name="analytics", key="price_target_requests", amount=1)
+
+
+async def update_locations_requests(client: Redis, location_id: int):
+    await client.hincrby(name="analytics:locations", key=str(location_id), amount=1)
+
+
+async def update_categories_requests(client: Redis, category_id: int):
+    await client.hincrby(name="analytics:categories", key=str(category_id), amount=1)
 
 
 storage_example = {
@@ -10,18 +25,9 @@ storage_example = {
     "discounts": set(),
 
     "analytics": {
-        "meta": {
-            'total_requests': 0,
-            'matrix_requests': 0,
-            'locations_requests': 0,
-            'categories_requests': 0,
-            'price_target_requests': 0,
-        },
-        'tables': {
-            'prices': {'rows': 0, 'updates': 0, 'deletions': 0, 'creations': 0},
-            'matrices': {'rows': 0, 'updates': 0, 'deletions': 0, 'creations': 0},
-            'locations': {'rows': 0, 'updates': 0, 'deletions': 0, 'creations': 0},
-            'categories': {'rows': 0, 'updates': 0, 'deletions': 0, 'creations': 0}
-        }
+        "price_target_requests": 0,
+
+        "locations": {"location_id": 0},
+        "categories": {"category_id": 0},
     }
 }
