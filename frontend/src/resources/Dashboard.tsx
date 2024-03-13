079677f14c71dd74d91fd9c5db165ed4ad463eb0
AlexandrVino <alexander-vin0@yandex.ru>
2024-03-14 01:18:24 +0300

    charts settings

--- frontend/src/resources/Dashboard.tsx
+++ frontend/src/resources/Dashboard.tsx
@@ -1,25 +1,23 @@
-import { Card, CardContent, CircularProgress, Typography } from '@mui/material'
-import { BarChart, LineChart, PieChart } from '@mui/x-charts'
+import {Box, Card, CardContent, CircularProgress, Typography} from '@mui/material'
+import {BarChart, LineChart, PieChart} from '@mui/x-charts'
 import React from 'react'
-import { Title } from 'react-admin'
-import { useQuery } from 'react-query'
-import { api } from '~/api/api'
+import {Title} from 'react-admin'
+import {useQuery} from 'react-query'
+import {api} from '~/api/api'
+import {green, purple} from "@mui/material/colors";
 
 const getAnalyticsInfo = async () => {
   return await api.get('storage/analytics')
 }
 
-const Dashboard = () => {  
-  const { data, isLoading, isError } = useQuery("dataStorage", () => getAnalyticsInfo());
+const Dashboard = () => {
+  const {data, isLoading, isError} = useQuery("dataStorage", () => getAnalyticsInfo());
   const obj = data?.data;
   if (isLoading || isError) {
-    return <CircularProgress />
+    return <CircularProgress/>
   }
-  console.log(obj?.total_requests);
-  const d = Object.keys(obj?.categories).map((key) => { return (key) })
-  console.log(d);
-  const dataset = Object.keys(obj.categories).map((key) => { return ({[key]: obj.categories[key]}) })
-  console.log(dataset);
+
+  console.log(obj);
 
   return (
     <Card>
@@ -27,41 +25,52 @@ const Dashboard = () => {
       <CardContent>
         {
           isLoading ?
-          <CircularProgress />
-          : isError ?
-          <Typography>Error!</Typography>
-          :
-          <div>
-            <BarChart
-              xAxis={[{ scaleType: 'band', data: ['group A', 'group B', 'group C'] }]}
-              series={[{ data: [4, 3, 5] }, { data: [1, 6, 3] }, { data: [2, 5, 6] }]}
-              width={500}
-              height={300}
-            />
-            <LineChart
-              xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
-              series={[
-                {
-                  data: [2, 5.5, 2, 8.5, 1.5, 5],
-                },
-              ]}
-              width={500}
-              height={300}
-            />
-            <PieChart
-              series={[
-                {
-                  data: [
-                    { id: 0, value: 10, label: 'series A' },
-                    { id: 1, value: 15, label: 'series B' },
-                    { id: 2, value: 20, label: 'series C' },
-                  ],
-                },
-              ]}
-              width={400}
-              height={200}
-            />
-          </div>
+            <CircularProgress/>
+            : isError ?
+              <Typography>Error!</Typography>
+              :
+              <Box sx={{display: "flex", flexWrap: "wrap", justifyContent: "space-between"}}>
+                <LineChart
+                  xAxis={[{
+                    scaleType: 'band',
+                    data: Object.keys(obj?.categories),
+                    tickLabelStyle: {angle: 90, textAnchor: 'start', fontSize: 15, fontWeight: 600}
+                  }]}
+                  yAxis={[{
+                    data: Object.values(obj?.categories),
+                    tickLabelStyle: {textAnchor: 'end', fontSize: 15, fontWeight: 600}
+                  }]}
+                  series={[{data: Object.values(obj?.categories), color:  '#DE196B'}]}
+                  width={600}
+                  height={400}
+                />
+                <LineChart
+                  xAxis={[{
+                    scaleType: 'band',
+                    data: Object.keys(obj?.locations),
+                    tickLabelStyle: {angle: 90, textAnchor: 'start', fontSize: 15, fontWeight: 600}
+                  }]}
+                  yAxis={[{
+                    data: Object.values(obj?.locations),
+                    tickLabelStyle: {textAnchor: 'end', fontSize: 15, fontWeight: 600}
+                  }]}
+                  series={[{data: Object.values(obj?.locations), color:  '#DE196B'}]}
+                  width={600}
+                  height={400}
+                />
+                <LineChart
+                  xAxis={[{
+                    scaleType: 'band',
+                    data: Object.keys(obj?.dates),
+                  }]}
+                  yAxis={[{
+                    data: Object.values(obj?.dates),
+                  }]}
+                  series={[{data: Object.values(obj?.dates), color:  '#DE196B'}]}
+                  width={600}
+                  height={400}
+                />
+              </Box>
         }
       </CardContent>
     </Card>
